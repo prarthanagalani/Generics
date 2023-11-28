751650170a9139d63ca7775b76ef79b45f77e379
prarthanagalani <prarthanagalani@gmail.com>
2023-11-28 20:48:10 +0530

    added: Uc2: find maximum among 3 float values-day8

--- FindMaxMin.java
+++ FindMaxMin.java
@@ -1,9 +1,25 @@
 package Generics;
 
 public class FindMaxMin {
-
+    
+    // Use Case 1: Find max of 3 integers
     public static Integer findMax(Integer num1, Integer num2, Integer num3) {
         Integer max = num1;
+
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+
+        return max;
+    }
+
+    // Use Case 2: Find max of 3 float values
+    public static Float findMax(Float num1, Float num2, Float num3) {
+        Float max = num1;
 
         if (num2.compareTo(max) > 0) {
             max = num2;
@@ -32,6 +48,18 @@ public class FindMaxMin {
         num2 = 8;
         num3 = 20;
         System.out.println("Test Case 3: " + findMax(num1, num2, num3));
+
+        // Test Case 1: Given Max Number at 1st Position, return the Same Number (Float)
+        Float floatNum1 = 15.5f, floatNum2 = 5.2f, floatNum3 = 8.9f;
+        System.out.println("Test Case 1 (Float): " + findMax(floatNum1, floatNum2, floatNum3));
+
+        // Test Case 2: Given Max Number at 2nd Position, return the Same Number (Float)
+        floatNum1 = 5.2f; floatNum2 = 15.7f; floatNum3 = 8.1f;
+        System.out.println("Test Case 2 (Float): " + findMax(floatNum1, floatNum2, floatNum3));
+
+        // Test Case 3: Given Max Number at 3rd Position, return the Same Number (Float)
+        floatNum1 = 5.4f; floatNum2 = 8.8f; floatNum3 = 20.3f;
+        System.out.println("Test Case 3 (Float): " + findMax(floatNum1, floatNum2, floatNum3));
     }
 
 }
