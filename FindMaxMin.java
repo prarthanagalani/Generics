6bcd9acc11f8f69bbde0616eb9e9ce489f86a23a
prarthanagalani <prarthanagalani@gmail.com>
2023-11-28 21:44:28 +0530

    refactored: Uc3: added generic method-day8

--- FindMaxMin.java
+++ FindMaxMin.java
@@ -1,47 +1,17 @@
 package Generics;
 
 public class FindMaxMin {
-    
-    // Use Case 1: Find max of 3 integers
-    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1;
 
-        if (num2.compareTo(max) > 0) {
-            max = num2;
+    // Generic method to find the max of three values
+    public static <T extends Comparable<T>> T findMax(T val1, T val2, T val3) {
+        T max = val1;
+
+        if (val2.compareTo(max) > 0) {
+            max = val2;
         }
 
-        if (num3.compareTo(max) > 0) {
-            max = num3;
-        }
-
-        return max;
-    }
-
-    // Use Case 2: Find max of 3 float values
-    public static Float findMax(Float num1, Float num2, Float num3) {
-        Float max = num1;
-
-        if (num2.compareTo(max) > 0) {
-            max = num2;
-        }
-
-        if (num3.compareTo(max) > 0) {
-            max = num3;
-        }
-
-        return max;
-    }
-
-     // Use Case 3: Find max of 3 strings
-     public static String findMax(String str1, String str2, String str3) {
-        String max = str1;
-
-        if (str2.compareTo(max) > 0) {
-            max = str2;
-        }
-
-        if (str3.compareTo(max) > 0) {
-            max = str3;
+        if (val3.compareTo(max) > 0) {
+            max = val3;
         }
 
         return max;
@@ -69,11 +39,15 @@ public class FindMaxMin {
         System.out.println("Test Case 1 (Float): " + findMax(floatNum1, floatNum2, floatNum3));
 
         // Test Case 2: Given Max Number at 2nd Position, return the Same Number (Float)
-        floatNum1 = 5.2f; floatNum2 = 15.7f; floatNum3 = 8.1f;
+        floatNum1 = 5.2f;
+        floatNum2 = 15.7f;
+        floatNum3 = 8.1f;
         System.out.println("Test Case 2 (Float): " + findMax(floatNum1, floatNum2, floatNum3));
 
         // Test Case 3: Given Max Number at 3rd Position, return the Same Number (Float)
-        floatNum1 = 5.4f; floatNum2 = 8.8f; floatNum3 = 20.3f;
+        floatNum1 = 5.4f;
+        floatNum2 = 8.8f;
+        floatNum3 = 20.3f;
         System.out.println("Test Case 3 (Float): " + findMax(floatNum1, floatNum2, floatNum3));
 
         // Test Case 1: Given Max String at 1st Position, return the Same String
@@ -81,11 +55,15 @@ public class FindMaxMin {
         System.out.println("Test Case 1 (String): " + findMax(str1, str2, str3));
 
         // Test Case 2: Given Max String at 2nd Position, return the Same String
-        str1 = "apple"; str2 = "orange"; str3 = "banana";
+        str1 = "apple";
+        str2 = "orange";
+        str3 = "banana";
         System.out.println("Test Case 2 (String): " + findMax(str1, str2, str3));
 
         // Test Case 3: Given Max String at 3rd Position, return the Same String
-        str1 = "banana"; str2 = "apple"; str3 = "orange";
+        str1 = "banana";
+        str2 = "apple";
+        str3 = "orange";
         System.out.println("Test Case 3 (String): " + findMax(str1, str2, str3));
     }
 
