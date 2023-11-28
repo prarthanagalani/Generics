aa876109240c4219066b30c80f9deaf25ec041b9
prarthanagalani <prarthanagalani@gmail.com>
2023-11-28 20:41:45 +0530

    added: Uc1: find maximum among 3 integers-day8

--- FindMaxMin.java
+++ FindMaxMin.java
@@ -2,9 +2,36 @@ package Generics;
 
 public class FindMaxMin {
 
-    public static void main(String[] args)
-    {
-        
+    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1;
+
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+
+        return max;
     }
-    
+
+    public static void main(String[] args) {
+        // Test Case 1: Given Max Number at 1st Position, return the Same Number
+        Integer num1 = 10, num2 = 5, num3 = 8;
+        System.out.println("Test Case 1: " + findMax(num1, num2, num3));
+
+        // Test Case 2: Given Max Number at 2nd Position, return the Same Number
+        num1 = 5;
+        num2 = 15;
+        num3 = 8;
+        System.out.println("Test Case 2: " + findMax(num1, num2, num3));
+
+        // Test Case 3: Given Max Number at 3rd Position, return the Same Number
+        num1 = 5;
+        num2 = 8;
+        num3 = 20;
+        System.out.println("Test Case 3: " + findMax(num1, num2, num3));
+    }
+
 }
