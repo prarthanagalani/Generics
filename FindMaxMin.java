7b47a064f4a86dfc5fd743ebaad51d940bb01cd0
prarthanagalani <prarthanagalani@gmail.com>
2023-11-29 09:24:40 +0530

    added: Uc4: now you can pass multiple parameters to find max-day8

--- FindMaxMin.java
+++ FindMaxMin.java
@@ -1,102 +1,60 @@
 package Generics;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class FindMaxMin<T extends Comparable<T>> {
 
-    private T val1, val2, val3;
+    private List<T> values;
 
     // Constructor
-    public FindMaxMin(T val1, T val2, T val3) {
-        this.val1 = val1;
-        this.val2 = val2;
-        this.val3 = val3;
+    public FindMaxMin(T... values) {
+        this.values = Arrays.asList(values);
     }
 
     // Generic method to find the max of three values
-    public T findMax() {
-        T max = val1;
-
-        if (val2.compareTo(max) > 0) {
-            max = val2;
+    public Optional<T> findMax() {
+        if(values.isEmpty())
+        {
+            return Optional.empty();
         }
 
-        if (val3.compareTo(max) > 0) {
-            max = val3;
-        }
+        Collections.sort(values,Collections.reverseOrder());
 
-        return max;
+        return Optional.of(values.get(0));
     }
 
     // Test method to call the static findMax method
     public void testMaximum() {
-        System.out.println("Maximum value: " + findMax());
+        System.out.println("Maximum value: " + findMax().orElse((T) "no maximum found."));
     }
 
     public static void main(String[] args) {
         // Test Case 1: Given Max Number at 1st Position, return the Same Number
-        Integer num1 = 10, num2 = 5, num3 = 8;
+       
         System.out.println("Test Case 1: ");
-        FindMaxMin<Integer> findmax = new FindMaxMin<Integer>(num1, num2, num3);
+        FindMaxMin<Integer> findmax = new FindMaxMin<Integer>(1,2,3,4,5);
         findmax.testMaximum();
 
-        // Test Case 2: Given Max Number at 2nd Position, return the Same Number
-        num1 = 5;
-        num2 = 15;
-        num3 = 8;
-        System.out.println("Test Case 2: ");
-        findmax = new FindMaxMin<Integer>(num1, num2, num3);
-        findmax.testMaximum();
-
-        // Test Case 3: Given Max Number at 3rd Position, return the Same Number
-        num1 = 5;
-        num2 = 8;
-        num3 = 20;
-        System.out.println("Test Case 3: ");
-        findmax = new FindMaxMin<Integer>(num1, num2, num3);
-        findmax.testMaximum();
+        
 
         // Test Case 1: Given Max Number at 1st Position, return the Same Number (Float)
-        Float floatNum1 = 15.5f, floatNum2 = 5.2f, floatNum3 = 8.9f;
+       
         System.out.println("Test Case 1 (Float): ");
-        FindMaxMin<Float> findmaxfloat = new FindMaxMin<Float>(floatNum1, floatNum2, floatNum3);
+        FindMaxMin<Float> findmaxfloat = new FindMaxMin<Float>(1.5f,4.5f,15.6f);
         findmaxfloat.testMaximum();
 
-        // Test Case 2: Given Max Number at 2nd Position, return the Same Number (Float)
-        floatNum1 = 5.2f;
-        floatNum2 = 15.7f;
-        floatNum3 = 8.1f;
-        System.out.println("Test Case 2 (Float): ");
-        findmaxfloat = new FindMaxMin<Float>(floatNum1, floatNum2, floatNum3);
-        findmaxfloat.testMaximum();
-
-        // Test Case 3: Given Max Number at 3rd Position, return the Same Number (Float)
-        floatNum1 = 5.4f;
-        floatNum2 = 8.8f;
-        floatNum3 = 20.3f;
-        System.out.println("Test Case 3 (Float): ");
-        findmaxfloat = new FindMaxMin<Float>(floatNum1, floatNum2, floatNum3);
-        findmaxfloat.testMaximum();
+       
 
         // Test Case 1: Given Max String at 1st Position, return the Same String
-        String str1 = "orange", str2 = "banana", str3 = "apple";
+       
         System.out.println("Test Case 1 (String): ");
-        FindMaxMin<String> findmaxstring = new FindMaxMin<String>(str1, str2, str3);
+        FindMaxMin<String> findmaxstring = new FindMaxMin<String>("cat","dog","car","apple","orange");
         findmaxstring.testMaximum();
 
-        // Test Case 2: Given Max String at 2nd Position, return the Same String
-        str1 = "apple";
-        str2 = "orange";
-        str3 = "banana";
-        System.out.println("Test Case 2 (String): ");
-        findmaxstring = new FindMaxMin<String>(str1, str2, str3);
-        findmaxstring.testMaximum();
-
-        // Test Case 3: Given Max String at 3rd Position, return the Same String
-        str1 = "banana";
-        str2 = "apple";
-        str3 = "orange";
-        System.out.println("Test Case 3 (String): ");
-        findmaxstring = new FindMaxMin<String>(str1, str2, str3);
-        findmaxstring.testMaximum();
+       
     }
 
 }
