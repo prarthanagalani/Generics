81ed97b1ed09a2d7c5c8ed2a7f48c08cce1350d6
prarthanagalani <prarthanagalani@gmail.com>
2023-11-29 08:44:44 +0530

    refactored2: Uc1: added generic class-day8

--- FindMaxMin.java
+++ FindMaxMin.java
@@ -1,9 +1,18 @@
 package Generics;
 
-public class FindMaxMin {
+public class FindMaxMin<T extends Comparable<T>> {
 
-     // Generic method to find the max of three values
-     public static <T extends Comparable<T>> T findMax(T val1, T val2, T val3) {
+    private T val1, val2, val3;
+
+    // Constructor
+    public FindMaxMin(T val1, T val2, T val3) {
+        this.val1 = val1;
+        this.val2 = val2;
+        this.val3 = val3;
+    }
+
+    // Generic method to find the max of three values
+    public T findMax() {
         T max = val1;
 
         if (val2.compareTo(max) > 0) {
@@ -17,22 +26,33 @@ public class FindMaxMin {
         return max;
     }
 
+    // Test method to call the static findMax method
+    public void testMaximum() {
+        System.out.println("Maximum value: " + findMax());
+    }
+
     public static void main(String[] args) {
         // Test Case 1: Given Max Number at 1st Position, return the Same Number
         Integer num1 = 10, num2 = 5, num3 = 8;
-        System.out.println("Test Case 1: " + findMax(num1, num2, num3));
+        System.out.println("Test Case 1: ");
+        FindMaxMin<Integer> findmax = new FindMaxMin<>(num1, num2, num3);
+        findmax.testMaximum();
 
         // Test Case 2: Given Max Number at 2nd Position, return the Same Number
         num1 = 5;
         num2 = 15;
         num3 = 8;
-        System.out.println("Test Case 2: " + findMax(num1, num2, num3));
+        System.out.println("Test Case 2: ");
+        findmax = new FindMaxMin<>(num1, num2, num3);
+        findmax.testMaximum();
 
         // Test Case 3: Given Max Number at 3rd Position, return the Same Number
         num1 = 5;
         num2 = 8;
         num3 = 20;
-        System.out.println("Test Case 3: " + findMax(num1, num2, num3));
+        System.out.println("Test Case 3: ");
+        findmax = new FindMaxMin<>(num1, num2, num3);
+        findmax.testMaximum();
     }
 
 }
