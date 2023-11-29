86b4ff83eb697ed41ab5a68d3d44366e8a489a19
prarthanagalani <prarthanagalani@gmail.com>
2023-11-29 09:32:45 +0530

    added: Uc5: Added method to print max that is callable from testmaximum()-day8

--- FindMaxMin.java
+++ FindMaxMin.java
@@ -15,46 +15,49 @@ public class FindMaxMin<T extends Comparable<T>> {
     }
 
     // Generic method to find the max of three values
-    public Optional<T> findMax() {
-        if(values.isEmpty())
-        {
-            return Optional.empty();
+    public T findMax() {
+        if (values.isEmpty()) {
+            return null;
         }
 
-        Collections.sort(values,Collections.reverseOrder());
+        Collections.sort(values, Collections.reverseOrder());
 
-        return Optional.of(values.get(0));
+        return values.get(0);
     }
 
     // Test method to call the static findMax method
     public void testMaximum() {
-        System.out.println("Maximum value: " + findMax().orElse((T) "no maximum found."));
+        T max = findMax();
+        if (max != null) {
+            printMax(max);
+        } else {
+            System.out.println("No maximum value found");
+        }
+    }
+
+    private void printMax(T max) {
+        System.out.println("Maximum value: " + max);
     }
 
     public static void main(String[] args) {
         // Test Case 1: Given Max Number at 1st Position, return the Same Number
-       
+
         System.out.println("Test Case 1: ");
-        FindMaxMin<Integer> findmax = new FindMaxMin<Integer>(1,2,3,4,5);
+        FindMaxMin<Integer> findmax = new FindMaxMin<Integer>(1, 2, 3, 4, 5);
         findmax.testMaximum();
 
-        
-
         // Test Case 1: Given Max Number at 1st Position, return the Same Number (Float)
-       
+
         System.out.println("Test Case 1 (Float): ");
-        FindMaxMin<Float> findmaxfloat = new FindMaxMin<Float>(1.5f,4.5f,15.6f);
+        FindMaxMin<Float> findmaxfloat = new FindMaxMin<Float>(1.5f, 4.5f, 15.6f);
         findmaxfloat.testMaximum();
 
-       
-
         // Test Case 1: Given Max String at 1st Position, return the Same String
-       
+
         System.out.println("Test Case 1 (String): ");
-        FindMaxMin<String> findmaxstring = new FindMaxMin<String>("cat","dog","car","apple","orange");
+        FindMaxMin<String> findmaxstring = new FindMaxMin<String>("cat", "dog", "car", "apple", "orange");
         findmaxstring.testMaximum();
 
-       
     }
 
 }
